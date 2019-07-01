6a80a347ce7fc7b456a9e802f2a7bedd947fc432
mat-krol <mateusz.damian.krol@gmail.com>
2019-07-01 14:20:00 +0100

    set up portfolio

--- src/components/block/portfolio/item/BlockPortfolioItem.js
+++ src/components/block/portfolio/item/BlockPortfolioItem.js
@@ -4,8 +4,15 @@ import classes from './BlockPortfolioItem.module.css'
 function BlockPortfolioItem(props) {
   return (
     <div className={classes.BlockPortfolioItem}>
+      <div className={classes.BlockPortfolioItemContent}>
+        <h2>Emmi.ai</h2>
+        <span>Static Pages</span>
+        <p>Gatsby, React, Photoshop</p>
+      </div>
     </div>
   )
 }
+
+
 
 export default BlockPortfolioItem
