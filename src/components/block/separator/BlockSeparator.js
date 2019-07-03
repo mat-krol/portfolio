3ff5a1738155f92cf5bc761d290f87eddbdad948
mat-krol <mateusz.damian.krol@gmail.com>
2019-07-03 09:49:34 +0100

    new inspo

--- src/components/block/separator/BlockSeparator.js
+++ src/components/block/separator/BlockSeparator.js
@@ -12,29 +12,29 @@ function BlockSeparator(props) {
             </clipPath>
           </defs>
             <g clipPath="url(#_clipPath_mN1cZdwAGGcYFGZO1uBwrrBeliFO0QWh)">
-              <polygon points="1220,310,-20,310,600,0" fill="rgb(255,255,255)" vector-effect="non-scaling-stroke" strokeWidth="1" stroke="rgb(36,66,189)" strokeLinejoin="miter" strokeLinecap="square" strokeMiterlimit="5"/>
+              <polygon points="1220,310,-20,310,600,0" fill="rgb(255,255,255)" vector-effect="non-scaling-stroke" strokeWidth="1" stroke={props.color} strokeLinejoin="miter" strokeLinecap="square" strokeMiterlimit="5"/>
             </g>
         </svg>
-        <h2 className={classes.BlockSeparatorText} id="#portfolio">Portfolio</h2>
+        <h2 className={classes.BlockSeparatorText} id={`#${props.title}`}>{props.title}</h2>
       </div>
     )
-  } else if (props.wave) {
+  } else if (props.wavelet) {
     return (
       <div className={classes.BlockSeparatorHolder}>
-        <svg viewBox="0 0 1050 90"  className={classes.BlockSeparator} preserveAspectRatio="none">
+        <svg viewBox="0 0 1050 90"  className={classes.BlockSeparator} style={{ height: "100px" }}preserveAspectRatio="none">
           <defs>
             <clipPath id="_clipPath_qDwIPkv1w9CWXokr2uuXreQN8YzRvHrU">
               <rect width="1050" height="90"/>
             </clipPath>
           </defs>
           <g clipPath="url(#_clipPath_qDwIPkv1w9CWXokr2uuXreQN8YzRvHrU)">
-            <path d=" M 0 45 C 57.724 102.724 91.95 103.05 150 45 C 208.05 -13.05 242.276 -12.724 300 45 C 357.724 102.724 389.512 105.488 450 45 C 510.488 -15.488 541.192 -13.808 600 45 C 658.808 103.808 692.601 102.399 750 45 C 807.399 -12.399 843.278 -11.722 900 45 C 956.722 101.722 990.108 104.892 1050 45" fill="none" vector-effect="non-scaling-stroke" strokeWidth="1" stroke="rgb(36,66,189)" strokeLinejoin="miter" strokeLinecap="square" strokeMiterlimit="3"/>
+            <path d=" M 0 45 C 57.724 102.724 91.95 103.05 150 45 C 208.05 -13.05 242.276 -12.724 300 45 C 357.724 102.724 389.512 105.488 450 45 C 510.488 -15.488 541.192 -13.808 600 45 C 658.808 103.808 692.601 102.399 750 45 C 807.399 -12.399 843.278 -11.722 900 45 C 956.722 101.722 990.108 104.892 1050 45" fill="none" vector-effect="non-scaling-stroke" strokeWidth="1" stroke={props.color} strokeLinejoin="miter" strokeLinecap="square" strokeMiterlimit="3"/>
           </g>
         </svg>
-        <h2 className={classes.BlockSeparatorText} id="#portfolio">Portfolio</h2>
+        <h2 className={classes.BlockSeparatorText} id={`#${props.title}`}>{props.title}</h2>
       </div>
     )
-  } else {
+  } else if (props.wave) {
     return (
       <div className={classes.BlockSeparatorHolder}>
         <svg viewBox="0 0 1200 250"  className={classes.BlockSeparator} preserveAspectRatio="none">
@@ -44,10 +44,26 @@ function BlockSeparator(props) {
             </clipPath>
           </defs>
           <g clipPath="url(#_clipPath_rrO3p6oswYn16OxuRyK4aYyqjvHaF3uw)">
-            <path d=" M 0 124 C 169.026 282.685 178.136 286.18 400 124 C 621.864 -38.18 578.864 -41.6 800 124 C 1021.136 289.6 987.535 285.938 1200 124" fill="none" vector-effect="non-scaling-stroke" strokeWidth="1" stroke="rgb(36,66,189)" strokeLinejoin="miter" strokeLinecap="square" strokeMiterlimit="3"/>
+            <path d=" M 0 124 C 169.026 282.685 178.136 286.18 400 124 C 621.864 -38.18 578.864 -41.6 800 124 C 1021.136 289.6 987.535 285.938 1200 124" fill="none" vector-effect="non-scaling-stroke" strokeWidth="1" stroke={props.color} strokeLinejoin="miter" strokeLinecap="square" strokeMiterlimit="3"/>
           </g>
         </svg>
-        <h2 className={classes.BlockSeparatorText} id="#portfolio">Contact</h2>
+        <h2 className={classes.BlockSeparatorText} id={`#${props.title}`}>{props.title}</h2>
+      </div>
+    )
+  } else {
+    return (
+      <div className={classes.BlockSeparatorHolder}>
+        <svg viewBox="0 0 1200 300" className={classes.BlockSeparator} preserveAspectRatio="none">
+          <defs>
+            <clipPath id="_clipPath_jwCRgmVI8Kozblx3Qo4hYjE17omIu82m">
+              <rect width="1200" height="300" />
+            </clipPath>
+          </defs>
+          <g clip-path="url(#_clipPath_jwCRgmVI8Kozblx3Qo4hYjE17omIu82m)">
+            <line x1="0" y1="80" x2="1200" y2="80" vector-effect="non-scaling-stroke" stroke-width="1" stroke={props.color} stroke-linejoin="miter" stroke-linecap="square" stroke-miterlimit="3" />
+          </g>
+        </svg>
+        <h2 className={classes.BlockSeparatorText} id={`#${props.title}`}>{props.title}</h2>
       </div>
     )
   }
--- src/components/view/home/ViewHome.js
+++ src/components/view/home/ViewHome.js
@@ -30,14 +30,15 @@ function ViewHome(props) {
       <SEO title="" keywords={[`mat krol`, `front end developer`, `software engineer`]} />
       <BlockHeader />
       <BlockBanner />
-      <BlockSeparator triangle />
+      <BlockSeparator wave title="portfolio" color="#2442bd" />
       <div style={{ position: "relative" }}>
         <BlockPortfolio handleShow={handleShow} />
         <BlockCard show={showing} handleClose={handleClose} image={pic} />
         <Backdrop show={showing} onClick={handleClose} />
       </div>
+      <BlockSeparator title="about" color="#2442bd" />
       <BlockAbout />
-      <BlockSeparator />
+      <BlockSeparator triangle title="contact" color="#2442bd" />
       <BlockContact />
       {/* <div className={classes.ViewHome}>
       </div> */}
