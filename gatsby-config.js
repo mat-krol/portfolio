bec954f3479ed58caf979c4b55a4c6e7f4e4b470
unknown <mateusz.damian.krol@gmail.com>
2021-03-20 20:45:32 +0000

    remove unused packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,8 +34,5 @@ module.exports = {
         head: true,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
