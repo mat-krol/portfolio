8ab9eeb62fbbf7b6a1a7f6514868fa1d9bbea8ce
Mateusz Krol <43745362+mat-krol@users.noreply.github.com>
2022-05-15 20:49:58 +0100

    set up metadata component

---
+++ src/components/MetaData.tsx
@@ -0,0 +1,62 @@
+import { graphql, useStaticQuery } from "gatsby";
+import React from "react";
+import Helmet from "react-helmet";
+
+type Props = {
+  description?: string;
+  title?: string;
+};
+
+export function MetaData({ description, title }: Props) {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+          }
+        }
+      }
+    `
+  );
+
+  const metaDescription = description || site.siteMetadata.description;
+
+  return (
+    <Helmet
+      title={title}
+      titleTemplate={`%s`}
+      meta={[
+        {
+          name: `description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:title`,
+          content: title,
+        },
+        {
+          property: `og:description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:type`,
+          content: `website`,
+        },
+        {
+          name: `twitter:card`,
+          content: `summary`,
+        },
+        {
+          name: `twitter:title`,
+          content: title,
+        },
+        {
+          name: `twitter:description`,
+          content: metaDescription,
+        },
+      ]}
+    />
+  );
+}
