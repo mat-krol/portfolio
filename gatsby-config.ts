2a4ac118c5bc7ecc31de7ada7412db4c6b248e0e
Mateusz Krol <43745362+mat-krol@users.noreply.github.com>
2022-05-15 20:55:23 +0100

    add a new favicon and manifest

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -18,6 +18,18 @@ const config: GatsbyConfig = {
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `portfolio`,
+        short_name: `portfolio`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#fff`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon.png`,
+      },
+    },
+    {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "images",
