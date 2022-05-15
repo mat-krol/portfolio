6e096ad0c523026ddc47cd8eec4e89f866db338f
Mateusz Krol <43745362+mat-krol@users.noreply.github.com>
2022-05-15 22:16:19 +0100

    update the copy with newest info

--- src/views/components/about/BlockAbout.tsx
+++ src/views/components/about/BlockAbout.tsx
@@ -10,24 +10,18 @@ export function BlockAbout() {
       <h2>About</h2>
       <div className={styles.BlockAboutCopy}>
         <span>
-          I have 1 year of professional experience in using React to create
-          responsive web apps and prototypes.
+          I have 4 years of professional experience in using React, TypeScript,
+          and Node.js to create full-stack apps and static sites
         </span>
-        <span>
-          And 3 years of non-professional experience in using WordPress, HTML,
-          CSS and Photoshop to create simple static pages.
-        </span>
-        {/* <p>My prior experience includes creating WordPress sites and web graphics for student societies</p> */}
       </div>
       <div className={styles.BlockAboutContent}>
-        <Progress title="HTML5" width="90%" />
-        <Progress title="CSS3" width="80%" />
-        <Progress title="JavaScript" width="55%" />
-        <Progress title="React" width="50%" />
-        <Progress title="Gatsby" width="60%" />
-        <Progress title="WordPress" width="80%" />
-        <Progress title="UX/UI" width="50%" />
-        <Progress title="Photoshop" width="90%" />
+        <Progress title="React" width="75%" />
+        <Progress title="JavaScript" width="90%" />
+        <Progress title="Next.js" width="75%" />
+        <Progress title="TypeScript" width="60%" />
+        <Progress title="Gatsby" width="75%" />
+        <Progress title="Node.js" width="80%" />
+        <Progress title="Figma" width="90%" />
       </div>
     </div>
   );
--- src/views/components/banner/BlockBanner.tsx
+++ src/views/components/banner/BlockBanner.tsx
@@ -1,27 +1,27 @@
 import React from "react";
 import { FaAngleDoubleDown } from "react-icons/fa";
 
+//@ts-ignore
 import * as styles from "./BlockBanner.module.css";
 
 export function BlockBanner() {
   return (
     <div className={styles.BlockBanner}>
       <div className={styles.BlockBannerNav}>
-        <a href="#portfolio">Portfolio</a>
+        <a href="#projects">Personal Projects</a>
+        <a href="#professional">Professional Experience</a>
         <a href="#about">About</a>
-        <a href="#contact">Contact</a>
       </div>
       <div>
         <h1>
           Hi! My name is <span>Mat Krol</span>
         </h1>
         <p>
-          I'm a web designer & front end developer with a passion for clean and
-          usable design
+          I'm a full stack engineer with a passion for clean and usable design
         </p>
       </div>
       <div>
-        <h2 id="portfolio">Portfolio</h2>
+        <h2 id="projects">Personal Projects</h2>
         <FaAngleDoubleDown className={styles.BlockBannerArrow} />
       </div>
     </div>
