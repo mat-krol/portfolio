541b34074dacd915bac515e7d25f7ce2570ddc7b
Mateusz Krol <43745362+mat-krol@users.noreply.github.com>
2022-05-15 20:50:13 +0100

    set up IndexPage using IndexView

---
+++ src/views/IndexView.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { MetaData } from "../components/MetaData";
+
+export function IndexView() {
+  return (
+    <>
+      <MetaData title="Mat Krol - Portfolio" />
+    </>
+  );
+}
