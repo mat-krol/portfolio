db24b6afaebce95f435ccb2f8efde06d21f538af
Mateusz Krol <43745362+mat-krol@users.noreply.github.com>
2022-05-16 00:40:03 +0100

    set up new separator

---
+++ src/views/IndexView/components/separator/Wavelet.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export function Wavelet() {
+  return (
+    <svg
+      viewBox="0 0 1050 90"
+      style={{ height: "35px" }}
+      preserveAspectRatio="none"
+    >
+      <defs>
+        <clipPath id="_clipPath_qDwIPkv1w9CWXokr2uuXreQN8YzRvHrU">
+          <rect width="1050" height="90" />
+        </clipPath>
+      </defs>
+      <g clipPath="url(#_clipPath_qDwIPkv1w9CWXokr2uuXreQN8YzRvHrU)">
+        <path
+          d=" M 0 45 C 57.724 102.724 91.95 103.05 150 45 C 208.05 -13.05 242.276 -12.724 300 45 C 357.724 102.724 389.512 105.488 450 45 C 510.488 -15.488 541.192 -13.808 600 45 C 658.808 103.808 692.601 102.399 750 45 C 807.399 -12.399 843.278 -11.722 900 45 C 956.722 101.722 990.108 104.892 1050 45"
+          fill="none"
+          vector-effect="non-scaling-stroke"
+          strokeWidth="1"
+          stroke="#2442bd"
+          strokeLinejoin="miter"
+          strokeLinecap="square"
+          strokeMiterlimit="3"
+        />
+      </g>
+    </svg>
+  );
+}
