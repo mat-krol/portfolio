c2a1f09063ae887593ea065e1a34a6d70bb48559
Mateusz Krol <43745362+mat-krol@users.noreply.github.com>
2022-05-15 23:29:09 +0100

    move lists into index

--- src/views/IndexView.tsx
+++ src/views/IndexView.tsx
@@ -13,9 +13,53 @@ export function IndexView() {
     <>
       <MetaData title="Mat Krol - Portfolio" />
       <BlockBanner />
-      <BlockPortfolio />
+      <BlockPortfolio list={projectList} />
+      <h2 id="professional">Professional Experience</h2>
+      <BlockPortfolio list={experienceList} />
       <BlockAbout />
       <BlockSeparator />
     </>
   );
 }
+
+const projectList = [
+  {
+    title: "Yoga with Ania",
+    subtitle: "Static Site",
+    tech: "Next.js",
+    src: "ywa/main.png",
+  },
+  {
+    title: "UWC Poland",
+    subtitle: "Static Site",
+    tech: "Next.js",
+    src: "uwc/main.png",
+  },
+  {
+    title: "Portfolio",
+    subtitle: "Site",
+    tech: "Gatsby",
+    src: "camclass",
+  },
+];
+
+const experienceList = [
+  {
+    title: "Academy",
+    subtitle: "Jun 2021 - now",
+    tech: "React, TypeScript, PostgreSQL, Next.js",
+    src: "camclass",
+  },
+  {
+    title: "Seneca Learning",
+    subtitle: "Oct 2019 - May 2021",
+    tech: "React + Redux, AWS, Node.js, Gatsby",
+    src: "emmi",
+  },
+  {
+    title: "OneMaths",
+    subtitle: "Oct 2018 - Jul 2019",
+    tech: "JavaScript, Ruby on Rails, React",
+    src: "om/main.png",
+  },
+];
