cd59aa0f1900c8c7dc5d306a85100e68826e6bce
Mateusz Krol <43745362+mat-krol@users.noreply.github.com>
2022-05-16 20:03:08 +0100

    restyle the grid

--- src/views/IndexView/components/portfolio/BlockPortfolio.tsx
+++ src/views/IndexView/components/portfolio/BlockPortfolio.tsx
@@ -1,35 +1,20 @@
-import { Grid, Stack, useBreakpointValue } from "@chakra-ui/react";
+import { Grid } from "@chakra-ui/react";
 import React from "react";
 
 import { PortfolioDescription } from "./components/PortfolioDescription";
-import { PortfolioImage } from "./components/PortfolioImage";
 
 type Props = { list: any[] };
 
 export function BlockPortfolio({ list }: Props) {
-  const noOfColumns = useBreakpointValue({
-    lg: 3,
-  });
-
-  if (noOfColumns === 3)
-    return (
-      <Grid gridTemplateColumns="1fr 1fr 1fr" columnGap={4}>
-        {list.map((item) => (
-          <PortfolioDescription key={item.src} {...item} />
-        ))}
-        {list.map((item) => (
-          <PortfolioImage key={item.src} src={item.src} />
-        ))}
-      </Grid>
-    );
-
   return (
-    <Grid gridTemplateColumns={{ base: "1fr", md: "1fr 1fr" }} rowGap={4}>
+    <Grid
+      gridTemplateColumns={{ base: "1fr", md: "1fr 1fr", lg: "1fr 1fr 1fr" }}
+      rowGap={{ base: 8, md: 4 }}
+      columnGap={4}
+      paddingX={{ base: 2, md: 0 }}
+    >
       {list.map((item) => (
-        <Stack spacing={0}>
-          <PortfolioDescription {...item} key={item.title} />
-          <PortfolioImage src={item.src} key={item.src} />
-        </Stack>
+        <PortfolioDescription {...item} key={item.title} />
       ))}
     </Grid>
   );
--- src/views/IndexView/components/portfolio/components/PortfolioDescription.tsx
+++ src/views/IndexView/components/portfolio/components/PortfolioDescription.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Center, Heading, Stack, Text } from "@chakra-ui/react";
+import { Button, Flex, Heading, Image, Stack, Text } from "@chakra-ui/react";
 import { Link } from "gatsby";
 import React from "react";
 
@@ -7,40 +7,46 @@ type Props = {
   subtitle: string;
   tech: string;
   slug?: string;
+  src: string;
 };
 
-export function PortfolioDescription({ title, subtitle, tech, slug }: Props) {
+export function PortfolioDescription({
+  title,
+  src,
+  subtitle,
+  tech,
+  slug,
+}: Props) {
   return (
-    <Box position="relative" paddingTop="57%">
-      <Center
-        top="0"
-        left="0"
-        background="gray.50"
-        width="100%"
-        height="100%"
-        position="absolute"
-      >
-        <Stack align="center" spacing={4}>
-          <Stack align="center" spacing={0}>
-            <Heading as="h3" size="md">
-              {title}
-            </Heading>
-            <Text fontSize="xs">{subtitle}</Text>
-          </Stack>
-          <Text>{tech}</Text>
-          <Link to={`/projects/${slug}`}>
-            <Button
-              colorScheme="blue"
-              backgroundColor="#2442bd"
-              fontWeight="normal"
-              _hover={{ backgroundColor: "#122987" }}
-              disabled={!slug}
-            >
-              Learn More
-            </Button>
-          </Link>
+    <Stack
+      spacing={4}
+      width="100%"
+      border={{ base: "none", md: "1px solid #ccc" }}
+      padding={4}
+    >
+      <Image src={src} />
+      <Text>{tech}</Text>
+
+      <Flex justifyContent="space-between" alignItems="baseline">
+        <Stack spacing={0}>
+          <Heading as="h3" size="md">
+            {title}
+          </Heading>
+          <Text fontSize="xs">{subtitle}</Text>
         </Stack>
-      </Center>
-    </Box>
+
+        <Link to={`/projects/${slug}`}>
+          <Button
+            colorScheme="blue"
+            backgroundColor="#2442bd"
+            fontWeight="normal"
+            _hover={{ backgroundColor: "#122987" }}
+            disabled={!slug}
+          >
+            Learn More
+          </Button>
+        </Link>
+      </Flex>
+    </Stack>
   );
 }
